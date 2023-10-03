22e771f66b6152ec3fa92c88af06eb94b4596b83
Philowu <146313659+Philowu@users.noreply.github.com>
2023-10-03 15:34:45 +0800

    完成基礎遊戲選單設置

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -5,24 +5,10 @@
 // 定義螢幕解析度
 #define SCREEN_WIDTH  800
 #define SCREEN_HEIGHT 600
+#define ID_CUSTOM_COMMAND 1001
+#define WM_CUSTOM_GAMEEND WM_USER + 1
 
-// 全域聲明
-HWND hWnd; // 只將W大寫避免錯誤呼叫
-//  按鈕宣告
-HWND Load_Button;
-HWND Clean_Button;
 
-// 函數原型
-void OpenFile(void);
-void DrawBitmap();
-void ClearDraw();
-void OnClick(int mouseX, int mouseY); //貪吃蛇使用不到
-
-//  宣告WindowProc
-LRESULT CALLBACK WindowProc(HWND hWnd,
-                         UINT message,
-                         WPARAM wParam,
-                         LPARAM lParam);
 
 //入口點
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -49,7 +35,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
     // 註冊視窗
     RegisterClassEx(&wc);
 
-    RECT wr = { 0, 0, 500, 400 };    // 设置尺寸，而不是位置
+    //根據客戶端取得視窗大小並做處理
+    RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };    // 设置尺寸，而不是位置
     AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);    // 調整大小
 
 // 建立窗口，並將傳回的結果作為句柄
@@ -115,13 +102,13 @@ int WINAPI WinMain(HINSTANCE hInstance,
     }
 
     // 清理 DirectX 和 COM
- //   CleanD3D();
+    // CleanD3D();
     // TODO: CleanD2D
     
     // 將WM_QUIT訊息的這一部分傳回給Windows
     return msg.wParam;
 }
-//按鈕點擊事件
+// 按鈕點擊事件
 void OnButtonClick(HWND hWnd)
 {
     MessageBox(hWnd, L"Button Clicked!", L"Button Click", MB_OK);
@@ -139,31 +126,51 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
             // 在此繪製按鈕會存在,但會被覆蓋,仍可以點選
             // 透過重繪事件,會先繪製背景再繪製按鈕
 
-
-            Load_Button = CreateWindow(
-                L"BUTTON",             // 按鈕控制項的類別名稱
-                L"Load",               // 按鈕上顯示的文字
+            Start_Button = CreateWindow(
+                L"BUTTON",                              // 按鈕控制項的類別名稱
+                L"開始遊戲",                            // 按鈕上顯示的文字
                 WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // 按鈕樣式
-                10, 10, 100, 30,        // 按鈕位置和大小 (x, y, width, height)
-                hWnd,                  // 父窗口句柄
-                (HMENU)1,              // 控制項 ID (可以用於識別按鈕)
-                GetModuleHandle(NULL), // 模組句柄
-                NULL                   // 指定為 NULL
+                10, 10, 100, 30,                        // 按鈕位置和大小 (x, y, width, height)
+                hWnd,                                   // 父窗口句柄
+                (HMENU)1,                               // 控制項 ID (可以用於識別按鈕)
+                GetModuleHandle(NULL),                  // 模組句柄
+                NULL                                    // 指定為 NULL
             );
-            Clean_Button = CreateWindow(
-                L"BUTTON",             // 按鈕控制項的類別名稱
-                L"Clean",              // 按鈕上顯示的文字
+            Difficulty_Button = CreateWindow(
+                L"BUTTON",                              // 按鈕控制項的類別名稱
+                L"難度選擇",                            // 按鈕上顯示的文字
                 WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // 按鈕樣式
-                120, 10, 100, 30,        // 按鈕位置和大小 (x, y, width, height)
-                hWnd,                  // 父窗口句柄
-                (HMENU)2,              // 控制項 ID (可以用於識別按鈕)
-                GetModuleHandle(NULL), // 模組句柄
-                NULL                   // 指定為 NULL
+                120, 10, 100, 30,                       // 按鈕位置和大小 (x, y, width, height)
+                hWnd,                                   // 父窗口句柄
+                (HMENU)2,                               // 控制項 ID (可以用於識別按鈕)
+                GetModuleHandle(NULL),                  // 模組句柄
+                NULL                                    // 指定為 NULL
+            );
+            Score_Button = CreateWindow(
+                L"BUTTON",                              // 按鈕控制項的類別名稱
+                L"歷史分數",                            // 按鈕上顯示的文字
+                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // 按鈕樣式
+                230, 10, 100, 30,                       // 按鈕位置和大小 (x, y, width, height)
+                hWnd,                                   // 父窗口句柄
+                (HMENU)3,                               // 控制項 ID (可以用於識別按鈕)
+                GetModuleHandle(NULL),                  // 模組句柄
+                NULL                                    // 指定為 NULL
+            );
+            End_Button = CreateWindow(
+                L"BUTTON",                              // 按鈕控制項的類別名稱
+                L"離開遊戲",                            // 按鈕上顯示的文字
+                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // 按鈕樣式
+                340, 10, 100, 30,                       // 按鈕位置和大小 (x, y, width, height)
+                hWnd,                                   // 父窗口句柄
+                (HMENU)4,                               // 控制項 ID (可以用於識別按鈕)
+                GetModuleHandle(NULL),                  // 模組句柄
+                NULL                                    // 指定為 NULL
             );
             // 使用2D繪圖 並觸發WM_PAINT後,按鈕即可正常顯示,故先不使用置頂設置
             // 猜測若使用置頂,但無觸發WM_PAINT,仍會發生按鈕被覆蓋的問題
             //SetWindowPos(Load_Button, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE); //使按鈕置頂
             //SetWindowPos(Clean_Button, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
             break;
         }
         case WM_COMMAND:
@@ -174,24 +181,30 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                 switch (LOWORD(wParam))
                 {
                 case 1: //TODO:: 開始遊戲
-                    OpenFile();
+                    ShowButton(0);
+                    OnPaint(hWnd);
                     break;
 
                 case 2: //TODO:: 難度選擇
-                    MessageBox(hWnd, L"Clean Clicked!", L"Clean Click", MB_OK);
-                    ClearDraw();
+                    MessageBox(hWnd, L"此功能尚未實作", L"錯誤", MB_OK | MB_ICONINFORMATION);
                     break;
 
                 case 3: //TODO:: 最高分數
-                    ClearDraw();
+                    MessageBox(hWnd, L"此功能尚未實作", L"錯誤", MB_OK | MB_ICONINFORMATION);
                     break;
 
-                case 4: //TODO:: 結束遊戲
-                    ClearDraw();
+                case 4: //離開遊戲
+                    //SendMessage(hWnd, WM_COMMAND, ID_CUSTOM_COMMAND, 0);  //此為自定義命令
+                    //SendMessage(hWnd, WM_CUSTOM_GAMEEND, 0, 0);           //此為自定義事件
+                    SendMessage(hWnd, WM_CLOSE, 0, 0);                      //視窗關閉
                     break;
+                case ID_CUSTOM_COMMAND: // 自定義命令的處理
+                    //MessageBox(hWnd, L"遊戲結束 \n得分為X", L"結算", MB_OK);
+                    return 0;
                 }
             }
             break;
+
         case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -200,15 +213,24 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
             DrawBitmap();
             EndPaint(hWnd, &ps);
         }break;
-        case WM_LBUTTONDOWN:
-            // 處理滑鼠左鍵
-            {
-                int xPos = GET_X_LPARAM(lParam);
-                int yPos = GET_Y_LPARAM(lParam);
-                OnClick(xPos, yPos);
-            }break;
+        //貪吃蛇不使用
+        //case WM_LBUTTONDOWN:
+        //    // 處理滑鼠左鍵
+        //    {
+        //        int xPos = GET_X_LPARAM(lParam);
+        //        int yPos = GET_Y_LPARAM(lParam);
+        //        OnClick(xPos, yPos);
+        //    }break;
 
-            // 當視窗關閉時會讀取此訊息
+
+        case WM_CUSTOM_GAMEEND:
+            // 處理遊戲結束
+            MessageBox(hWnd, L"遊戲結束 \n得分為X", L"結算", MB_OK);
+            ShowButton(1);
+            ClearDraw();
+            break;
+
+        // 當視窗關閉時會讀取此訊息
         case WM_DESTROY:
             {
                 if(pRT!=NULL)
@@ -234,7 +256,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 ////////////////////////////////////////////////////////////////
 
 
-
+// 讀取檔案並轉換成圖片
 HRESULT LoadBitmapFromFile(ID2D1RenderTarget* pRenderTarget, IWICImagingFactory* pIWICFactory, LPCWSTR uri, UINT destinationWidth, UINT destinationHeight, ID2D1Bitmap** ppBitmap)
 {
     // 初始化 WIC
@@ -286,6 +308,7 @@ HRESULT LoadBitmapFromFile(ID2D1RenderTarget* pRenderTarget, IWICImagingFactory*
     return hr;
 }
 
+// 開啟檔案
 void OpenFile()
 {
     OPENFILENAME ofn;
@@ -303,7 +326,7 @@ void OpenFile()
     ofn.nMaxFileTitle = 0;
     ofn.lpstrInitialDir = NULL;
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-    //TODO:判斷副檔名
+    // TODO:判斷副檔名
     if (GetOpenFileName(&ofn) == TRUE)
     {
         IWICImagingFactory* pIWICFactory = NULL;
@@ -313,7 +336,7 @@ void OpenFile()
     }
 }
 
-
+// 繪圖
 void DrawBitmap()
 {
     if (pRT && pBitmap)
@@ -326,6 +349,7 @@ void DrawBitmap()
         pRT->EndDraw();
     }
 }
+
 void ClearDraw()
 {
     pRT->BeginDraw();
@@ -354,4 +378,40 @@ void OnClick(int mouseX, int mouseY)
     // 通知系統進行重繪
     InvalidateRect(hWnd, NULL, TRUE);
 
+}
+
+void OnPaint(HWND hWnd)
+{
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+
+    // 使用Direct2D繪製四邊形
+    pRT->BeginDraw();
+    pRT->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+
+    D2D1_RECT_F rectangle = D2D1::RectF(1.0f, 1.0f, SCREEN_WIDTH-3, SCREEN_HEIGHT-3);
+    ID2D1SolidColorBrush* pBlackBrush;
+    pRT->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Blue), &pBlackBrush);
+    pRT->DrawRectangle(&rectangle, pBlackBrush, 7.0f);
+
+    pRT->EndDraw();
+
+    EndPaint(hWnd, &ps);
+}
+void ShowButton(bool show)
+{
+    if(show)
+    { 
+        ShowWindow(Start_Button, SW_SHOW);
+        ShowWindow(Difficulty_Button, SW_SHOW);
+        ShowWindow(Score_Button, SW_SHOW);
+        ShowWindow(End_Button, SW_SHOW);
+    }
+    else
+    {
+        ShowWindow(Start_Button, SW_HIDE);
+        ShowWindow(Difficulty_Button, SW_HIDE);
+        ShowWindow(Score_Button, SW_HIDE);
+        ShowWindow(End_Button, SW_HIDE);
+    }
 }
--- Project2/mian.h
+++ Project2/mian.h
@@ -8,7 +8,29 @@
 
 #ifndef MIAN
 #define MIAN
+// 全域聲明
+HWND hWnd; // 只將W大寫避免錯誤呼叫
+//  按鈕宣告
+HWND Load_Button;
+HWND Clean_Button;
+HWND Start_Button;
+HWND Difficulty_Button;
+HWND Score_Button;
+HWND End_Button;
 
+// 函數原型
+void OpenFile(void);
+void DrawBitmap();
+void ClearDraw();
+void OnClick(int mouseX, int mouseY); //貪吃蛇使用不到
+void OnPaint(HWND hWnd);
+void ShowButton(bool);
+
+//  宣告WindowProc
+LRESULT CALLBACK WindowProc(HWND hWnd,
+                            UINT message,
+                            WPARAM wParam,
+                            LPARAM lParam);
 
 
 #endif // MIAN
